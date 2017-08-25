44ec3f8792569dfb8d05eaadf3a0e9ab8587204f
Matthew Holman <mateoholman@gmail.com>
2017-08-25 16:16:33 -0500

    Updated boilerplate to include a header, navbar and utilize redux for auth

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,47 @@
 //The Header / Menu
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-const Header = () => {
-  return <div>Header</div>;
-};
+class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Log In</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a>Logout</a>
+          </li>
+        );
+    }
+  }
 
-export default Header;
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a href="/" className="left brand-logo">
+            emcamp
+          </a>
+          <ul className="right">
+            <li>
+              {this.renderContent()}
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
